1af8673a07dab3608b36036f91d1c3bb89b611b8
umutakinc <umutakinci5@gmail.com>
2024-07-05 22:21:30 +0300

    getComputerChoice function add

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log(1);
+function getComputerChoice() {
+    let choices = ["taş", "kağıt", "makas"]; // Taş kağıt makas dizelerinin tutulduğu dizi 
+    let randomNumber = Math.random() * choices.length; // En büyük dizinin uzunluğu olabilecek random sayı
+    randomNumber = Math.floor(randomNumber); // Ondalıklı sayıyı yuvarlıyoruz
+    let computerChoice = choices[randomNumber]; // Rastgele seçilen dizi değeri 
+
+    return computerChoice;
+}
+
+getComputerChoice();
