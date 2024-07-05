e21065b29fd454339350429ba6730412e8e0df1c
umutakinc <umutakinci5@gmail.com>
2024-07-05 23:27:07 +0300

    playRound function add

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// Kazananı belirleyecek skor değişkenleri
+var humanScore = 0, computerScore = 0;
+
 function getComputerChoice() {
     let choices = ["Taş", "Kağıt", "Makas"]; // Taş kağıt makas dizelerinin tutulduğu dizi 
     let randomNumber = Math.random() * choices.length; // En büyük dizinin uzunluğu olabilecek random sayı
@@ -8,7 +11,30 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let promptChoice = prompt('Taş, Kağıt ve Makas değerlerinden birini giriniz.');
+    let promptChoice = prompt('Taş, Kağıt ve Makas değerlerinden birini giriniz.'); // Oyuncunun seçtiği seçenek
+    promptChoice = promptChoice.charAt(0).toLocaleUpperCase() + promptChoice.slice(1).toLocaleLowerCase(); // Oyuncudan aldığımız dizeyi ilk harf büyük diğerleri küçük olacak şekilde formatlıyoruz
 
     return promptChoice;
 }
+
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        console.log('Berabere kaldınız');
+    } else if (
+        (humanChoice == "Kağıt" && computerChoice == "Taş") 
+        || 
+        (humanChoice == "Taş" && computerChoice == "Makas") 
+        || 
+        (humanChoice == "Makas" && computerChoice == "Kağıt")
+    ) {
+        console.log(`Kazandınız! ${humanChoice} ${computerChoice}'ı yener.`);
+    } else {
+        computerScore = computerScore++;
+        console.log(`Kaybettiniz! ${computerChoice} ${humanChoice}'ı yener`);
+    }
+}
+
+playRound(humanSelection, computerSelection);
